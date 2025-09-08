31bad70cb08c44219810c75c3423c5f4aa58da24
colieen <colineel514@gmail.com>
2025-09-08 11:27:59 +0800

    Update detect.py

--- src/detect.py
+++ src/detect.py
@@ -158,10 +158,10 @@ try:
 
                 if servo_sequence is None and stable_counter >= 3:  # wait 3 frames
                     if red_area > green_area:
-                        servo_sequence = ([(1000,1), (1500,1), (2000,1), (1500,0)], 0, current_time + 0.1)
+                        servo_sequence = ([(2000,1), (1500,1), (1000,1), (1500,0)], 0, current_time + 0.1)
                         color_detected = red_label
                     elif green_area > red_area:
-                        servo_sequence = ([(2000,1), (1500,1), (1000,1), (1500,0)], 0, current_time + 0.1)
+                        servo_sequence = ([(1000,1), (1500,1), (2000,1), (1500,0)], 0, current_time + 0.1)
                         color_detected = green_label
                     else:
                         pwm.set_servo_pulsewidth(servo_pin, 1500)
@@ -207,3 +207,4 @@ finally:
     motorSpeed(0)
     pwm.set_servo_pulsewidth(servo_pin, 0)
     cv2.destroyAllWindows()
+
