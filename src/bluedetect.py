25fd8bfb93dcd1e6535dfe8573c6113e7c690a93
ubmay (raspberry pi) <alphonsus.mn@gmail.com>
2025-09-10 14:55:04 +0800

    add bluedetect add visual bars to centering (doesn't work now)

---
+++ src/bluedetect.py
@@ -0,0 +1,181 @@
+# Run sudo pigpiod in terminal before running code
+
+from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import time, sleep
+import pigpio
+import numpy as np
+import cv2
+from picamera2 import Picamera2
+
+pi = pigpio.pi()
+last_detected = None
+turn_amt = -200
+offset = -50
+
+# Ultrasonic sensors
+factory = PiGPIOFactory()
+frontultrasonic = DistanceSensor(echo=5, trigger=6, max_distance=4, pin_factory=factory)  # White (22) and Brown (27)
+leftultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=4, pin_factory=factory)   # Orange (17) and Blue (4)
+rightultrasonic = DistanceSensor(echo=27, trigger=22, max_distance=4, pin_factory=factory)   # Green (6) and Blue (5)
+backultrasonic = DistanceSensor(echo=19, trigger=26, max_distance=4, pin_factory=factory)  # White (19) and Purple (26)
+
+# Servo
+servo_pin = 16
+pwm = pigpio.pi()
+pwm.set_mode(servo_pin, pigpio.OUTPUT)
+pwm.set_PWM_frequency(servo_pin, 50)
+pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)  # neutral start
+
+# Camera
+cam = Picamera2()
+cam.configure(cam.create_preview_configuration({"size": (640, 360)}))
+cam.start()
+
+# Motors
+M1A = 23
+M1B = 24
+pi.set_mode(M1A, pigpio.OUTPUT)
+pi.set_mode(M1B, pigpio.OUTPUT)
+
+last_orange_seen = -1
+# Orange line detection
+def detect_orange(frame, on_orange_line, lap_count):
+    global last_orange_seen
+
+    hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    orange_lower = np.array([5, 150, 150], np.uint8)
+    orange_upper = np.array([20, 255, 255], np.uint8)
+    orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
+
+    height, width = orange_mask.shape
+    roi = orange_mask[height-60:height, :]
+    orange_pixels = cv2.countNonZero(roi)
+
+    crossed = False
+    if orange_pixels > 1000:
+        if not on_orange_line and (time() - last_orange_seen) > 1.6:
+            lap_count += 1
+            crossed = True
+            on_orange_line = True
+            last_orange_seen = time()
+    else:
+        on_orange_line = False
+
+    contours, _ = cv2.findContours(orange_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        largest = max(contours, key=cv2.contourArea)
+        if cv2.contourArea(largest) > 300:
+            x, y, w, h = cv2.boundingRect(largest)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 165, 255), 2)
+
+    return lap_count, on_orange_line, crossed
+
+
+last_blue_seen = -1
+# Orange line detection
+def detect_blue(frame, on_blue_line, lap_count):
+    global last_blue_seen
+
+    hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    blue_lower = np.array([100, 150, 50], np.uint8)
+    blue_upper = np.array([130, 255, 255], np.uint8)
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+
+    height, width = blue_mask.shape
+    roi = blue_mask[height-60:height, :]
+    blue_pixels = cv2.countNonZero(roi)
+
+    crossed = False
+    if blue_pixels > 1000:
+        if not on_blue_line and (time() - last_blue_seen) > 1.6:
+            lap_count += 1
+            crossed = True
+            on_blue_line = True
+            last_blue_seen = time()
+    else:
+        on_blue_line = False
+
+    contours, _ = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        largest = max(contours, key=cv2.contourArea)
+        if cv2.contourArea(largest) > 300:
+            x, y, w, h = cv2.boundingRect(largest)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 165, 255), 2)
+
+    return lap_count, on_blue_line, crossed
+
+# Motor speed
+def motorSpeed(speed):
+    if speed < 0:
+        pi.set_PWM_dutycycle(M1A, abs(speed))
+        pi.set_PWM_dutycycle(M1B, 0)
+    elif speed > 0:
+        pi.set_PWM_dutycycle(M1A, 0)
+        pi.set_PWM_dutycycle(M1B, abs(speed))
+    else:
+        pi.set_PWM_dutycycle(M1A, 0)
+        pi.set_PWM_dutycycle(M1B, 0)
+
+
+lap_count = 0
+on_orange_line = False
+orange_sequence = None  
+
+try:
+    while True:
+        frame = cv2.cvtColor(cam.capture_array()[::-1, :, :3], cv2.COLOR_RGB2BGR)
+        distance_cm = frontultrasonic.distance * 100
+        current_time = time()
+
+        if lap_count < 12:
+            # --- ORANGE LINE DETECTION ---
+            lap_count, on_orange_line, crossed = detect_blue(frame, on_orange_line, lap_count)
+            motorSpeed(100)
+
+            if crossed and orange_sequence is None:
+                orange_sequence = ([(1500 + offset, 0.0), ((1500 + offset + turn_amt), 3.01), (1500 + offset, 0)], 0, current_time)
+                print(f"Lap {lap_count} completed")
+
+            # --- ORANGE SEQUENCE HANDLING ---
+            if orange_sequence is not None:
+                steps, step_index, start_time = orange_sequence
+                pos, duration = steps[step_index]
+                pwm.set_servo_pulsewidth(servo_pin, pos)
+
+                if current_time - start_time >= duration:
+                    step_index += 1
+                    if step_index >= len(steps):
+                        orange_sequence = None
+                    else:
+                        orange_sequence = (steps, step_index, current_time)
+
+            # --- ULTRASONIC BACKUP (crash avoidance) ---
+            if distance_cm < 30 and orange_sequence is None:
+                pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)
+                left_dist = leftultrasonic.distance * 100
+                right_dist = rightultrasonic.distance * 100
+                if left_dist > right_dist and left_dist > 20:
+                    pwm.set_servo_pulsewidth(servo_pin, (1500 + offset + turn_amt))
+                elif right_dist > left_dist and right_dist > 20:
+                    pwm.set_servo_pulsewidth(servo_pin, (1500 + offset - turn_amt))
+                else:
+                    pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)
+                    motorSpeed(0)
+
+        else:
+            pwm.set_servo_pulsewidth(servo_pin, 1500 + offset + turn_amt)
+            sleep(2.5)
+            pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)
+            motorSpeed(0)
+            break
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            motorSpeed(0)
+            break
+
+finally:
+    motorSpeed(0)
+    pwm.set_servo_pulsewidth(servo_pin, 0)
+    cv2.destroyAllWindows()
+
--- src/centering.py
+++ src/centering.py
@@ -1,26 +1,81 @@
+from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import time, sleep
+import pigpio
+
+pi = pigpio.pi()
+
+
+# Ultrasonic sensors
+factory = PiGPIOFactory()
+frontultrasonic = DistanceSensor(echo=5, trigger=6, max_distance=4, pin_factory=factory)
+leftultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=4, pin_factory=factory)
+rightultrasonic = DistanceSensor(echo=27, trigger=22, max_distance=4, pin_factory=factory)
+backultrasonic = DistanceSensor(echo=19, trigger=26, max_distance=4, pin_factory=factory)
+turn_amt = 200
+offset = -50
+
+# Servo
+servo_pin = 16
+pwm = pigpio.pi()
+pwm.set_mode(servo_pin, pigpio.OUTPUT)
+pwm.set_PWM_frequency(servo_pin, 50)
+pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)  
+
+M1A = 23
+M1B = 24
+pi.set_mode(M1A, pigpio.OUTPUT)
+pi.set_mode(M1B, pigpio.OUTPUT)
+
+
+def motorSpeed(speed):
+    if speed < 0:
+        pi.set_PWM_dutycycle(M1A, abs(speed))
+        pi.set_PWM_dutycycle(M1B, 0)
+    elif speed > 0:
+        pi.set_PWM_dutycycle(M1A, 0)
+        pi.set_PWM_dutycycle(M1B, abs(speed))
+    else:
+        pi.set_PWM_dutycycle(M1A, 0)
+        pi.set_PWM_dutycycle(M1B, 0)
+
+frame = 0
 def align_center(left_target, right_target, tolerance):
+    global frame
     while True:
         left_dist = leftultrasonic.distance * 100
         right_dist = rightultrasonic.distance * 100
       
         if abs(left_dist - left_target) <= tolerance and abs(right_dist - right_target) <= tolerance:
             motorSpeed(0)
-            pwm.set_servo_pulsewidth(servo_pin, 1500)  # straighten
-            break
+            pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)  # straighten
 
         if left_dist < left_target:
-            pwm.set_servo_pulsewidth(servo_pin, 2000)
-            motorSpeed(80)
+            pwm.set_servo_pulsewidth(servo_pin, 1500 + offset + turn_amt)
+            motorSpeed(60)
 
         elif right_dist < right_target:
-            pwm.set_servo_pulsewidth(servo_pin, 1000)
-            motorSpeed(80)
+            pwm.set_servo_pulsewidth(servo_pin, 1500 + offset - turn_amt)
+            motorSpeed(60)
 
         else:
             # If both distances are bigger, just go straight and let sensors stabilize
-            pwm.set_servo_pulsewidth(servo_pin, 1500)
-            motorSpeed(80)
+            pwm.set_servo_pulsewidth(servo_pin, 1500 + offset)
+            motorSpeed(60)
+
+        left_dist /= 100
+        right_dist /= 100
+        print(f"Left:  {left_dist:.2f} m", "#" * int(left_dist * 20), "\033[K")
+        print(f"Right: {right_dist:.2f} m", "#" * int(right_dist * 20), "\033[K")
+        print("frame %d" % frame)
 
         # Small delay to avoid overshoot
         time_sleep = 0.1
         sleep(time_sleep)
+        # print("\033[3A\r", end="")
+        frame += 1
+
+
+motorSpeed(60)
+sleep(0.5)
+align_center(49, 47, 5)
